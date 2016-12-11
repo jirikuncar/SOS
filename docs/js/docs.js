b1c03b190eba3d9610043923880fa2fd968457d1
Bo Peng <ben.bog@gmail.com>
2016-12-11 17:57:34 -0600

    Separate spyder kernel from sos kernel

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,2 +1,2 @@
 var documentation=["Auxiliary_Steps","Command_Line_Options","Configuration_Files","Extending_SoS","External_task","SoS_Functions","SoS_Kernel","SoS_Step","SoS_Syntax","String_Interpolation","User_Interface","Workflow_Specification"]
-var tutorials=["Quick_Start_Guide","SoS_Docker_Guide","Using_SoS_with_IPython","Using_SoS_with_Spyder"]
+var tutorials=["Execution_of_Workflow","Project_Management","Quick_Start_Guide","SoS_Docker_Guide","Using_SoS_with_IPython","Using_SoS_with_Spyder"]
