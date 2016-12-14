06ba6726aea8cb36aee1930b84cf1a2ba88fa7ad
Ma <jma7@BSTMC07N607BFW5.local>
2016-12-14 15:32:49 -0600

    add homepage

--- docs/js/sos_script.js
+++ docs/js/sos_script.js
@@ -1,34 +1,37 @@
 $(document).ready(function(){
-	$("#intro_content").load("https://raw.githubusercontent.com/BoPeng/SOS/master/docs/doc/README.html"); 
+	$("#intro_content").load("https://raw.githubusercontent.com/BoPeng/SOS/master/docs/doc/homepage/Overview.html"); 
+	$("#installation_content").load("https://raw.githubusercontent.com/BoPeng/SOS/master/docs/doc/homepage/Running_SoS.html"); 
+	$("#documentation_content").load("https://raw.githubusercontent.com/BoPeng/SOS/master/docs/doc/homepage/Documentation.html"); 
+
 	
-	var tuts=tutorials
-	$("#tutorial > .container").append('<div class="row">')
-	for (var a =0;a<tuts.length;a++){
-		var name =tuts[a];
-		var oneString='<div class="col-md-4 col-sm-6 portfolio-item">'
-        				+'<div class="portfolio-caption">';
-		oneString+='<a href="./doc/tutorials/'+name+'.html" class="portfolio-link"><h4>'+name+'</h4></a>';
-		oneString+='</div></div>';       	
-		 $("#tutorial > .container").append(
-	       	oneString
-	     );
-	}	
-	$("#tutorial > .container").append('</div>');	
+	// var tuts=tutorials
+	// $("#tutorial > .container").append('<div class="row">')
+	// for (var a =0;a<tuts.length;a++){
+	// 	var name =tuts[a];
+	// 	var oneString='<div class="col-md-4 col-sm-6 portfolio-item">'
+ //        				+'<div class="portfolio-caption">';
+	// 	oneString+='<a href="./doc/tutorials/'+name+'.html" class="portfolio-link"><h4>'+name+'</h4></a>';
+	// 	oneString+='</div></div>';       	
+	// 	 $("#tutorial > .container").append(
+	//        	oneString
+	//      );
+	// }	
+	// $("#tutorial > .container").append('</div>');	
 
 
- 	var docs=documentation
-	$("#documentation > .container").append('<div class="row">')
-	for (var a =0;a<docs.length;a++){
-		var name =docs[a];
-		var oneString='<div class="col-md-4 col-sm-6 portfolio-item">'
-        				+'<div class="portfolio-caption">';
-		oneString+='<a href="./doc/documentation/'+name+'.html" class="portfolio-link"><h4>'+name+'</h4></a>';
-		oneString+='</div></div>';       	
-		 $("#documentation > .container").append(
-	       	oneString
-	     );
-	}	
-	$("#documentation > .container").append('</div>');		
+ // 	var docs=documentation
+	// $("#documentation > .container").append('<div class="row">')
+	// for (var a =0;a<docs.length;a++){
+	// 	var name =docs[a];
+	// 	var oneString='<div class="col-md-4 col-sm-6 portfolio-item">'
+ //        				+'<div class="portfolio-caption">';
+	// 	oneString+='<a href="./doc/documentation/'+name+'.html" class="portfolio-link"><h4>'+name+'</h4></a>';
+	// 	oneString+='</div></div>';       	
+	// 	 $("#documentation > .container").append(
+	//        	oneString
+	//      );
+	// }	
+	// $("#documentation > .container").append('</div>');		
 
 
 
